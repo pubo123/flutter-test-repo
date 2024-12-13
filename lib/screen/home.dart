ae267bb7a16a177c365c31a9f9e5eb3a259921d9
pubo123 <puboleengu@gmail.com>
2024-12-13 17:25:43 +0800

    first commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home_Screen extends StatelessWidget {
+  const Home_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
